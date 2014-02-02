2f132d3026031d149abb76ba2b65597bf0675ae2
김거엽 <gasbank@bluehole.net>
2014-02-02 11:17:48 +0900

    파일 정리

--- Assets/Scripts/TitleMenu.cs
+++ Assets/Scripts/TitleMenu.cs
@@ -17,7 +17,8 @@ public class TitleMenu : MonoBehaviour {
 		if (GUI.Button(new Rect (Screen.width / 2 + 15, Screen.height / 2 + 40, 110, 30), " Start", BtnSkin.button)) {
 			Application.LoadLevel(1);
 		}
-		if (GUI.Button (new Rect (Screen.width / 2 + 15, Screen.height / 2 + 70, 110, 30), "End", BtnSkin.button)) {
-		}
+
+        //if (GUI.Button (new Rect (Screen.width / 2 + 15, Screen.height / 2 + 70, 110, 30), "End", BtnSkin.button)) {
+        //}
 	}
 }
