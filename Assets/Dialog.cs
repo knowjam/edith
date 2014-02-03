716d24a97be3801eaf736f41ca27f37fa4bf6ec3
김거엽 <gasbank@bluehole.net>
2014-02-04 01:58:06 +0900

    GUI 기본 테스트, 엔딩 연출 테스트

---
+++ Assets/Dialog.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : MonoBehaviour
+{
+    public GUISkin skin;
+    public string[] text;
+    public string buttonText;
+    public Texture tex;
+    public bool destoryAfterRead = true;
+    private bool dialogOn;
+    private int index;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (dialogOn)
+        {
+            
+            GUILayout.BeginArea(new Rect(0, 3*Screen.height/4, Screen.width, Screen.height/4), skin.textArea);
+
+            GUILayout.Label(text[index], skin.label);
+            if (GUILayout.Button(buttonText, skin.button))
+            {
+                ++index;
+
+                if (text.Length <= index)
+                {
+                    dialogOn = false;
+
+                    if (destoryAfterRead)
+                    {
+                        Destroy(gameObject);
+                    }
+                }
+            }
+
+            GUILayout.EndArea();
+
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), tex);
+
+        }
+    }
+
+    void OnTriggerEnter2D()
+    {
+        dialogOn = true;
+        index = 0;
+    }
+
+    void OnTriggerExit2D()
+    {
+        dialogOn = false;
+    }
+}
---
+++ Assets/ImageShower.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageShower : MonoBehaviour
+{
+    public Texture[] tex;
+    private Texture curTex;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (curTex)
+        {
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), curTex);
+        }
+    }
+
+    void OnTriggerEnter2D()
+    {
+        StartCoroutine("ShowSequence");
+    }
+
+    void OnTriggerExit2D()
+    {
+    }
+
+    IEnumerator ShowSequence()
+    {
+        foreach (var t in tex)
+        {
+            curTex = t;
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        //curTex = null;
+    }
+}
