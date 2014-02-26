b2ff4b462c144144b611f5a8f0cdc3da40b74763
김거엽 <gasbank@bluehole.net>
2014-02-27 01:38:35 +0900

    톱니바퀴 애셋 추가!

---
+++ Assets/CogwheelRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CogwheelRotate : MonoBehaviour
+{
+    public float speed = 10;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, speed*Time.deltaTime);
+    }
+}
