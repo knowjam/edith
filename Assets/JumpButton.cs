a913133f298c5e3ef77577678d21c9448e8c41d5
김거엽 <gasbank@bluehole.net>
2014-03-15 02:46:31 +0900

    안드로이드 UI 구현

---
+++ Assets/JumpButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpButton : MonoBehaviour
+{
+    public static bool jumpButtonHit;
+
+    void Update()
+    {
+        jumpButtonHit = false;
+        foreach (var t in Input.touches)
+        {
+            if (guiTexture.HitTest(t.position))
+            {
+                jumpButtonHit = true;
+                break;
+            }
+        }
+    }
+}
