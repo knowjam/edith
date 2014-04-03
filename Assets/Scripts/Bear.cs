4fca64fb5ce241238cc4b2f5076911a05a7e746f
김거엽 <gasbank@bluehole.net>
2014-04-04 02:20:01 +0900

    공격 범위 나오도록 수정, 코드 정리

--- Assets/Scripts/Bear.cs
+++ Assets/Scripts/Bear.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Bear : MonoBehaviour
 {
     public GameObject particle;
---
+++ Assets/Scripts/PlayerAnimEventHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimEventHandler : MonoBehaviour
+{
+    public GameObject itemAttackRangeObject;
+
+    void OnAnimIdle()
+    {
+        itemAttackRangeObject.GetComponent<TrailRenderer>().enabled = false;
+    }
+
+    void OnAnimEventAttackHitStart()
+    {
+        itemAttackRangeObject.GetComponent<TrailRenderer>().enabled = true;
+    }
+
+    void OnAnimEventAttackHitTime()
+    {
+        var player = GameObject.Find("Player").GetComponent<Player>();
+
+        if (player.playerMode == Player.PlayerMode.NotDetermined)
+        {
+            var frontObject = player.GetItemAttackRayCheckResult();
+
+            if (frontObject)
+            {
+                if (frontObject.CompareTag("RopeObject"))
+                {
+                    player.SendMessage("getSnake");
+                    GameObject.Destroy(frontObject);
+                }
+                else if (frontObject.CompareTag("GhostObject"))
+                {
+                    player.SendMessage("getGhost");
+                    GameObject.Destroy(frontObject);
+                }
+            }
+        }
+    }
+
+}
